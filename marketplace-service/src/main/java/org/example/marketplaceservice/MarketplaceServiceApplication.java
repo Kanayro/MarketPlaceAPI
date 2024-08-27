515b6dfe6470768bfb9fa26057642f17aad12d37
Alexander Kalinkov <skalinkov54@gmail.com>
2024-08-27 15:55:33 +0300

    Create marketplace-service

---
+++ marketplace-service/src/main/java/org/example/marketplaceservice/MarketplaceServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.marketplaceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketplaceServiceApplication.class, args);
+    }
+
+}
---
+++ marketplace-service/src/test/java/org/example/marketplaceservice/MarketplaceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.marketplaceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
