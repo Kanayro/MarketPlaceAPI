e6637f1e32988ae96ed78dd4b534f9b9ccef6f2d
Alexander Kalinkov <skalinkov54@gmail.com>
2024-08-27 22:57:34 +0300

    Add PersonMapper"

--- marketplace-service/src/main/java/org/example/marketplaceservice/MarketplaceServiceApplication.java
+++ marketplace-service/src/main/java/org/example/marketplaceservice/MarketplaceServiceApplication.java
@@ -1,13 +1,20 @@
 package org.example.marketplaceservice;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MarketplaceServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MarketplaceServiceApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
     }
 
 }
---
+++ marketplace-service/src/main/java/org/example/marketplaceservice/mappers/PersonMapper.java
@@ -0,0 +1,26 @@
+package org.example.marketplaceservice.mappers;
+
+import org.example.marketplaceservice.dto.PersonDTO;
+import org.example.marketplaceservice.models.Person;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PersonMapper {
+
+    private final ModelMapper mapper;
+
+    @Autowired
+    public PersonMapper(ModelMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    public PersonDTO convertToPersonDTO(Person person) {
+       return mapper.map(person, PersonDTO.class);
+    }
+
+    public Person convertToPerson(PersonDTO personDTO) {
+        return mapper.map(personDTO, Person.class);
+    }
+}
