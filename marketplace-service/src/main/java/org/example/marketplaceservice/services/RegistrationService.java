3a14df672bc6e0f44bf410630c552c6d0414ed5c
Alexander Kalinkov <skalinkov54@gmail.com>
2024-08-27 17:50:59 +0300

    Create RegistrationService

---
+++ marketplace-service/src/main/java/org/example/marketplaceservice/services/RegistrationService.java
@@ -0,0 +1,28 @@
+package org.example.marketplaceservice.services;
+
+import org.example.marketplaceservice.models.Person;
+import org.example.marketplaceservice.repositories.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class RegistrationService {
+
+    private final PersonRepository personRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public RegistrationService(PersonRepository personRepository, PasswordEncoder passwordEncoder) {
+        this.personRepository = personRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Transactional
+    public void register(Person person) {
+        person.setPassword(passwordEncoder.encode(person.getPassword()));
+
+        personRepository.save(person);
+    }
+}
