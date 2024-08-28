12c98db22b31a997e7bc7fc1fdd143917c7cbf5b
Alexander Kalinkov <skalinkov54@gmail.com>
2024-08-28 14:58:37 +0300

    add product

---
+++ marketplace-service/src/main/java/org/example/marketplaceservice/models/Product.java
@@ -0,0 +1,78 @@
+package org.example.marketplaceservice.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Product")
+public class Product {
+
+    @Id
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price")
+    private int price;
+
+    @Column(name = "count")
+    private int count;
+
+    @Column(name = "isCount")
+    private boolean isCount;
+
+    public Product() {
+    }
+
+    public Product(int id, String name, int price, int count, boolean isCount) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.count = count;
+        this.isCount = isCount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public boolean isCount() {
+        return isCount;
+    }
+
+    public void setCount(boolean count) {
+        isCount = count;
+    }
+}
